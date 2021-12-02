f68db6b51d6e17cb1678a21ba73c14dad24aaea7
Max <mxcs02@gmail.com>
2021-12-02 19:01:19 +0200

    Configure JWT.

---
+++ Models/JwtConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Marketplace.Models
+{
+	public class JwtConfiguration
+	{
+		public string? Issuer { get; set; }
+		public string? Audience { get; set; }
+		public string? Lifetime { get; set; }
+		public string? Key { get; set; }
+
+		public SymmetricSecurityKey GetSymmetricSecurityKey()
+		{
+			return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Key ?? throw new NullReferenceException()));
+		}
+		public TimeSpan GetTimeSpanLifetime()
+		{
+			return TimeSpan.FromMinutes(double.Parse(Lifetime ?? throw new NullReferenceException()));
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,48 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Marketplace.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<MarketplaceContext>(options =>
-	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+	options.UseSqlServer(ConfigurationExtensions.GetConnectionString(builder.Configuration, "DefaultConnection")));
 
-builder.Services.AddIdentity<User, IdentityRole<int>>()
+builder.Services.AddIdentity<User, IdentityRole<int>>(opts => {
+		opts.Password.RequireNonAlphanumeric = false;
+		opts.Password.RequireLowercase = false;
+		opts.Password.RequireUppercase = false;
+		opts.Password.RequireDigit = false;
+	})
 	.AddEntityFrameworkStores<MarketplaceContext>();
+
+var jwtConfigSection = builder.Configuration.GetSection(nameof(JwtConfiguration));
+var jwtConfig = jwtConfigSection.Get<JwtConfiguration>();
+
+builder.Services.Configure<JwtConfiguration>(jwtConfigSection);
+builder.Services.AddOptions();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+	.AddJwtBearer(options => {
+		options.RequireHttpsMetadata = false;
+		options.TokenValidationParameters = new TokenValidationParameters {
+			ValidateIssuer = true,
+			ValidateAudience = true,
+			ValidateLifetime = true,
+			ValidateIssuerSigningKey = true,
+			ValidIssuer = jwtConfig.Issuer,
+			ValidAudience = jwtConfig.Audience,
+			IssuerSigningKey = jwtConfig.GetSymmetricSecurityKey(),
+			ClockSkew = TimeSpan.Zero
+		};
+	});
+builder.Services.AddAuthorization(options => {
+	options.DefaultPolicy = new AuthorizationPolicyBuilder(JwtBearerDefaults.AuthenticationScheme)
+		.RequireAuthenticatedUser()
+		.Build();
+});
 
 builder.Services.AddControllersWithViews();
 
@@ -33,6 +66,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
 	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
+	pattern: "{controller=Home}/{action=Index}/{id?}"
+);
 
 app.Run();
