31a4caf45d8fb16e0f9db7dd58d13f7c8652f8e1
Max <mxcs02@gmail.com>
2022-03-17 01:25:37 +0200

    Remove temporary initialization

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,20 +1,14 @@
 ﻿using System.Diagnostics;
-using Marketplace.Models;
 using Marketplace.ViewModels;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Marketplace.Controllers
 {
 	public class HomeController : Controller
 	{
-		private readonly ILogger<HomeController> _logger;
-		private UserManager<User> userManager;
-
-		public HomeController(ILogger<HomeController> logger, UserManager<User> userManager)
+		public HomeController()
 		{
-			_logger = logger;
-			this.userManager = userManager;
+			
 		}
 
 		public IActionResult Index()
@@ -24,17 +18,6 @@ namespace Marketplace.Controllers
 
 		public async Task<IActionResult> Privacy()
 		{
-			await userManager.CreateAsync(
-				new User() {
-					UserName = "a",
-					Email = "a",
-					PhoneNumber = "555555",
-					Name = "max",
-					Created = DateTime.UtcNow,
-					CityId = 1
-				},
-				"123456"
-			);
 			return View();
 		}
 
